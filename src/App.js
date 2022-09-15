f4152ba0fa9b66010e8dc6e4160911f7f663cffe
jinsol <js2@nineonesoft.com>
2022-09-15 17:03:02 +0900

    Apply Font to MainPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//1. 앱실행 > 현재 위치 기반의 날씨 정보 나타내기
+//2. 날씨 정보에는 도시,섭씨/화씨, 날씨 상태
+//3. 5개 버튼 (1개는 현재 위치/4개는 다른 도시)
+//4. 도시 버튼을 클릭할 때 마다 도시별 날씨 나옴
+//5. 현재위치 버튼 > 다시 현재 위치 기반의 날씨가 나옴
+//6. 데이터를 가져오는 동안은 로딩 스피너가 보이게 셋팅
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hiiii
     </div>
   );
 }
